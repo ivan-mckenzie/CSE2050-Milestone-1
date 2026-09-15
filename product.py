cddfa9219e849ac2ddb66d778593774ecd201044
Ivan Mckenzie <ivanmckenzie_@outlook.com>
2026-09-15 12:04:47 -0400

    fixed errors, completed store and product

--- product.py
+++ product.py
@@ -1,12 +1,17 @@
 class Product:
+    """Stores an item sold by the store"""
     def __init__(self, product_id: str, name: str, price:float):
+        """Creates a product with a given id, name, and price"""
         self.product_id = product_id
         self.name = name
         self.price = price
     def get_id(self):
+        """Returns a product's id"""
         return self.product_id
     def get_name(self):
+        """Returns a product's name"""
         return self.name
     def get_price(self):
+        """Returns a product's price as a float"""
         return self.price
     
---
+++ store.py
@@ -0,0 +1,40 @@
+from product import Product
+from customer import Customer
+
+class Store:
+    """Class for the store itself that contains customers and products"""
+    def __init__(self):
+        """Creates a store with empty product and customer lists"""
+        self.products = list[Product] = []
+        self.customers = list[Customer] = []
+
+    def add_product(self, product: Product):
+        """Add a product if its ID is not already stored"""
+        if self.find_product(product.get_id()) is not None:
+            return False
+        else:
+            self.products.append(product)
+            return True
+        
+    def find_product(self, product_id: str):
+        """Find and return a product by id"""
+        for product in self.products:
+            if product.get_id() == product_id:
+                return product
+            else:
+                return None
+            
+    def add_customer(self, customer: Customer):
+        if self.find_customer(customer.get_id()) is not None:
+            return False
+        else:
+            self.Customer.append(customer)
+            return True
+        
+    def find_customer(self, customer_id: str):
+        """Find and return a customer by id"""
+        for customer in self.customers:
+            if customer.get_id() == customer_id:
+                return customer
+            else:
+                return None
